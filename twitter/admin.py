fe4c645b863596422a0c5de496fd46923995201a
Çağrı Karaosmanoğlu <ackaraosman@gmail.com>
2015-05-17 20:26:09 +0300

    Improve tweet admin

--- twitter/admin.py
+++ twitter/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Tweet
 
 
-admin.site.register(Tweet)
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['place_name', 'point', 'username', 'body']
