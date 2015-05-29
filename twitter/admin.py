1e109fc51f15417a4456aaf5389fa080016361bc
Çağrı Karaosmanoğlu <ackaraosman@gmail.com>
2015-05-29 23:41:09 +0300

    Add some more actions to admin

--- twitter/admin.py
+++ twitter/admin.py
@@ -13,6 +13,26 @@ def mark_as_not_train(modeladmin, request, queryset):
 mark_as_not_train.short_description = 'Remove from train data'
 
 
+def mark_as_train_hakaret(modeladmin, request, queryset):
+    queryset.update(klass='A', train=True)
+mark_as_train_hakaret.short_description = 'Mark as Train/Hakaret'
+
+
+def mark_as_train_irkcilik(modeladmin, request, queryset):
+    queryset.update(klass='I', train=True)
+mark_as_train_irkcilik.short_description = 'Mark as Train/Irkçılık'
+
+
+def mark_as_train_homofobi(modeladmin, request, queryset):
+    queryset.update(klass='H', train=True)
+mark_as_train_homofobi.short_description = 'Mark as Train/Homofobi'
+
+
+def mark_as_train_notr(modeladmin, request, queryset):
+    queryset.update(klass='N', train=True)
+mark_as_train_notr.short_description = 'Mark as Train/Notr'
+
+
 def mark_as_hakaret(modeladmin, request, queryset):
     queryset.update(klass='A')
 mark_as_hakaret.short_description = 'Mark as Hakaret'
@@ -44,12 +64,16 @@ class TweetAdmin(admin.OSMGeoAdmin):
     search_fields = ['body']
     actions = [
         mark_as_train,
-        mark_as_not_train,
+        mark_as_train_hakaret,
+        mark_as_train_irkcilik,
+        mark_as_train_homofobi,
+        mark_as_train_notr,
         mark_as_hakaret,
         mark_as_irkcilik,
         mark_as_homofobi,
-        unmark_classification,
         mark_as_notr,
+        mark_as_not_train,
+        unmark_classification,
     ]
 
 
