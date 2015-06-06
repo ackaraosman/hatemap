6d62e56fb2077b567fe79340918b97917d640894
Çağrı Karaosmanoğlu <ackaraosman@gmail.com>
2015-06-06 22:23:54 +0300

    Fetch the data which will be shown first

--- twitter/static/js/main.js
+++ twitter/static/js/main.js
@@ -66,6 +66,36 @@ $(function() {
   var pageParams = {k: 'hakaret', t: 'svm'};
   $.extend(pageParams, parseQuerystr(window.location.hash.substring(1)));
 
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: [],
+    radius: 20,
+    maxIntensity: 10
+  });
+
+  function fetchData() {
+    xhr = $.get(baseUrl + createQuerystr(pageParams), function(data) {
+      heatmap.setData(mapLatLng(data.results));
+      $('#loading-layer').hide();
+    });
+  }
+
+  fetchData();
+  renderMap();
+  setInterval(fetchData, 3000);
+
+
+  function renderMap() {
+    var turkey = new google.maps.LatLng(38.611, 34.831);
+
+    googleMap = new google.maps.Map($('#map-canvas')[0], {
+      center: turkey,
+      zoom: 6,
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+    });
+
+    heatmap.setMap(googleMap);
+  }
+
 
   // set active links initially
   for (var p in pageParams) {
@@ -123,36 +153,5 @@ $(function() {
   });
 
 
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: [],
-    radius: 20,
-    maxIntensity: 10
-  });
-
-
-  function renderMap() {
-    var turkey = new google.maps.LatLng(38.611, 34.831);
-
-    googleMap = new google.maps.Map($('#map-canvas')[0], {
-      center: turkey,
-      zoom: 6,
-      mapTypeId: google.maps.MapTypeId.SATELLITE,
-    });
-
-    heatmap.setMap(googleMap);
-  }
-
-
-  function fetchData() {
-    xhr = $.get(baseUrl + createQuerystr(pageParams), function(data) {
-      heatmap.setData(mapLatLng(data.results));
-      $('#loading-layer').hide();
-    });
-  }
-
-  renderMap();
-  fetchData();
-
-  setInterval(fetchData, 3000);
   new Spinner(spinnerOpts).spin($('#spinner')[0]);
 });
