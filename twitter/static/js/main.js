d8ed28ba5426e20bc5f46291c0add9afe1d49c7a
Çağrı Karaosmanoğlu <ackaraosman@gmail.com>
2015-06-06 21:55:44 +0300

    Add keyword filter

--- twitter/static/js/main.js
+++ twitter/static/js/main.js
@@ -1,3 +1,27 @@
+var spinnerOpts = {
+    lines: 11            // The number of lines to draw
+  , length: 5            // The length of each line
+  , width: 3             // The line thickness
+  , radius: 12           // The radius of the inner circle
+  , scale: 1             // Scales overall size of the spinner
+  , corners: 0.8         // Corner roundness (0..1)
+  , color: '#fff'        // #rgb or #rrggbb or array of colors
+  , opacity: 0.25        // Opacity of the lines
+  , rotate: 0            // The rotation offset
+  , direction: 1         // 1: clockwise, -1: counterclockwise
+  , speed: 1.5           // Rounds per second
+  , trail: 41            // Afterglow percentage
+  , fps: 20              // Frames per second when using setTimeout() as a fallback for CSS
+  , zIndex: 2e9          // The z-index (defaults to 2000000000)
+  , className: 'spinner' // The CSS class to assign to the spinner
+  , top: '50%'           // Top position relative to parent
+  , left: '50%'          // Left position relative to parent
+  , shadow: true         // Whether to render a shadow
+  , hwaccel: true        // Whether to use hardware acceleration
+  , position: 'absolute' // Element positioning
+};
+
+
 function parseQuerystr(qstr) {
   if (qstr === "") return {};
   var params = qstr.split('&');
@@ -71,12 +95,31 @@ $(function() {
     xhr.abort();
     var href = $(this).attr('href').substring(1);
     $.extend(pageParams, parseQuerystr(href));
-    heatmap.setData(initialData[pageParams.t][pageParams.k]);
+    if (pageParams.q) {
+      $('#loading-layer').show();
+      fetchData();
+    } else {
+      heatmap.setData(initialData[pageParams.t][pageParams.k]);
+    }
     window.location.hash = createQuerystr(pageParams);
 
     // set active class
     $(this).siblings().removeClass('active');
     $(this).addClass('active');
+  });
+
+
+  // keyword filter
+  $('#keyword-filter').click(function(e) {
+    e.preventDefault();
+    var q = $('#keyword-input').val();
+    if (q) {
+      xhr.abort();
+      pageParams.q = q;
+      window.location.hash = createQuerystr(pageParams);
+      $('#loading-layer').show();
+      fetchData();
+    }
   });
 
 
@@ -103,6 +146,7 @@ $(function() {
   function fetchData() {
     xhr = $.get(baseUrl + createQuerystr(pageParams), function(data) {
       heatmap.setData(mapLatLng(data.results));
+      $('#loading-layer').hide();
     });
   }
 
@@ -110,4 +154,5 @@ $(function() {
   fetchData();
 
   setInterval(fetchData, 3000);
+  new Spinner(spinnerOpts).spin($('#spinner')[0]);
 });
